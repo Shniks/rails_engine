22ac50ed94f9438a7b271c687b07e249eaab6064
Nikhil Shahi <shniks@gmail.com>
2018-05-01 07:49:33 -0600

    Created passing test for transaction by its invoice_id.

--- spec/requests/api/v1/transactions/search_request_spec.rb
+++ spec/requests/api/v1/transactions/search_request_spec.rb
@@ -6,14 +6,11 @@ describe "Transactions/Find API" do
 
     get "/api/v1/transactions/find?invoice_id=#{transactions[2].invoice_id}"
 
-    binding.pry
-
-    merchant = JSON.parse(response.body)
+    transaction = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(merchant["id"]).to eq(merchant_2.id)
-    expect(merchant["id"]).to_not eq(merchant_1.id)
-    expect(merchant["name"]).to eq(merchant_2.name)
-    expect(merchant["name"]).to_not eq(merchant_3.name)
+    expect(transaction["id"]).to eq(transactions[2].id)
+    expect(transaction["invoice_id"]).to eq(transactions[2].invoice_id)
+    expect(transaction["credit_card_number"]).to eq(transactions[2].credit_card_number)
   end
 end
