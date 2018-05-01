474fcd6a9df0d520f7afc65e208bc90231154a30
Nikhil Shahi <shniks@gmail.com>
2018-05-01 08:06:15 -0600

    Created passing test for transaction by result.

--- spec/requests/api/v1/transactions/search_request_spec.rb
+++ spec/requests/api/v1/transactions/search_request_spec.rb
@@ -42,4 +42,20 @@ describe "Transactions/Find API" do
     expect(transaction["invoice_id"]).to eq(transaction_2.invoice_id)
     expect(transaction["credit_card_number"]).to eq(transaction_2.credit_card_number)
   end
+
+  it "Can find a transaction by its result" do
+    expiration_date_1 = "2018-05-01 07:19:34"
+    expiration_date_2 = "2017-05-01 07:19:34"
+    transaction_1 = Transaction.create!(invoice_id: 2, credit_card_number: "34", credit_card_expiration_date: expiration_date_1, result: "success")
+    transaction_2 = Transaction.create!(invoice_id: 76, credit_card_number: "24", credit_card_expiration_date: expiration_date_2, result: "failure")
+
+    get "/api/v1/transactions/find?result=#{transaction_2.result}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(transaction["id"]).to eq(transaction_2.id)
+    expect(transaction["invoice_id"]).to eq(transaction_2.invoice_id)
+    expect(transaction["credit_card_number"]).to eq(transaction_2.credit_card_number)
+  end
 end
