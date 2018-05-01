db286c105735fa0e31777054ea31c932f237be1e
Nikhil Shahi <shniks@gmail.com>
2018-04-30 20:14:45 -0600

    Created test to find a merchant by name.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -23,4 +23,18 @@ describe "Merchants API" do
     expect(response).to be_successful
     expect(merchant["id"]).to eq(id)
   end
+
+  it "Can find a merchant by its name" do
+    merchant_1 = Merchant.create(name: "Sergio")
+    merchant_2 = Merchant.create(name: "Nikhil")
+    merchant_3 = Merchant.create(name: "Tyler")
+
+    get "/api/v1/merchants/find?name=Nikhil"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(merchant_2.id)
+    expect(merchant["name"]).to eq(merchant_2.name)
+  end
 end
