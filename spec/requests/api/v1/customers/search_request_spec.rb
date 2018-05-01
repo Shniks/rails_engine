b0a70d90071d6b500b457f17058b4a1ece523b49
Nikhil Shahi <shniks@gmail.com>
2018-05-01 13:54:09 -0600

    Created passing test for customers find by last name.

--- spec/requests/api/v1/customers/search_request_spec.rb
+++ spec/requests/api/v1/customers/search_request_spec.rb
@@ -13,4 +13,17 @@ describe "Customers/Find API" do
     expect(customer["first_name"]).to eq(customers[4].first_name)
     expect(customer["last_name"]).to eq(customers[4].last_name)
   end
+
+  it "Can find a customer by its last name" do
+    customers = create_list(:customer, 7)
+
+    get "/api/v1/customers/find?last_name=#{customers[4].last_name}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["id"]).to eq(customers[4].id)
+    expect(customer["first_name"]).to eq(customers[4].first_name)
+    expect(customer["last_name"]).to eq(customers[4].last_name)
+  end
 end
