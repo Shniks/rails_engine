d3b7b64fbed39a129d4a817b4156e43934d72393
Nikhil Shahi <shniks@gmail.com>
2018-05-01 07:50:42 -0600

    Created passing test to find transaction by its credit card number.

--- spec/requests/api/v1/transactions/search_request_spec.rb
+++ spec/requests/api/v1/transactions/search_request_spec.rb
@@ -13,4 +13,17 @@ describe "Transactions/Find API" do
     expect(transaction["invoice_id"]).to eq(transactions[2].invoice_id)
     expect(transaction["credit_card_number"]).to eq(transactions[2].credit_card_number)
   end
+
+  it "Can find a transaction by its credit_card_number" do
+    transactions = create_list(:transaction, 5)
+
+    get "/api/v1/transactions/find?credit_card_number=#{transactions[2].credit_card_number}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(transaction["id"]).to eq(transactions[2].id)
+    expect(transaction["invoice_id"]).to eq(transactions[2].invoice_id)
+    expect(transaction["credit_card_number"]).to eq(transactions[2].credit_card_number)
+  end
 end
