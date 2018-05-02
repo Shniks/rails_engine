d60e3b323294bc529e1acc2cec9e65c10bd4d22b
Nikhil Shahi <shniks@gmail.com>
2018-05-02 07:54:29 -0600

    Added merchant_name for each of the items in the item serializer.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,11 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :unit_price, :merchant_id, :merchant_name
 
   def unit_price
     '%.2f' % (object.unit_price.to_f / 100)
+  end
+
+  def merchant_name
+    object.merchant.name
   end
 end
