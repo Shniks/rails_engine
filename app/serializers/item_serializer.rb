542e273de30c67813d13cd9a2c076c8aa6720f9d
Nikhil Shahi <shniks@gmail.com>
2018-05-02 08:33:09 -0600

    Updated the serializers to conform to the spec harness.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,11 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :unit_price, :merchant_id, :merchant_name
+  attributes :id, :name, :description, :unit_price, :merchant_id
 
   def unit_price
     '%.2f' % (object.unit_price.to_f / 100)
-  end
-
-  def merchant_name
-    object.merchant.name
   end
 end
--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,9 +1,3 @@
 class MerchantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :num_items
-
-  has_many :items
-
-  def num_items
-    object.items.count
-  end
+  attributes :id, :name
 end
