4633713f56b1ef9c1c838e6314e68630fac5a1e7
Nikhil Shahi <shniks@gmail.com>
2018-05-03 05:58:57 -0600

    Created passing test for invoice invoice items relationship.

--- spec/requests/api/v1/invoices/relationships_request_spec.rb
+++ spec/requests/api/v1/invoices/relationships_request_spec.rb
@@ -10,9 +10,23 @@ describe 'Invoices API Relationships' do
 
     get "/api/v1/invoices/#{@invoice.id}/transactions"
 
-    expect(response).to be_successful
-
     transactions = JSON.parse(response.body)
+
+    expect(response).to be_successful
     expect(transactions.count).to eq(5)
+  end
+
+  it 'Returns a collection of associated invoice items' do
+    create_list(:invoice_item, 4, invoice: @invoice)
+
+    get "/api/v1/invoices/#{@invoice.id}/invoice_items"
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_items.count).to eq(4)
+    invoice_items.each do |invoice_item|
+      expect(invoice_item["invoice_id"]).to eq(@invoice.id)
+    end
   end
 end
