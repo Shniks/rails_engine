334325fd5047d9d23411ba24418dec57ed9a48c2
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 11:02:30 -0600

    Fix spec so Customer->Transactions relationship endpoint is validated

--- spec/requests/api/v1/customers/transactions_spec.rb
+++ spec/requests/api/v1/customers/transactions_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 describe 'Customer->Transactions API' do
   it 'should return a collection of a customers transactions' do
     customer_1, customer_2 = create_list(:customer, 2)
-    create_list(:invoice, 3, customer: customer_1)
+    invoices = create_list(:invoice, 3, customer: customer_1)
     create_list(:invoice, 5, customer: customer_2)
+
+    invoices.each do |invoice|
+      create(:transaction, invoice: invoice)
+    end
 
     get "/api/v1/customers/#{customer_1.id}/transactions"
 
