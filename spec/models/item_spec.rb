51368934a1ef926b8054eafe22296e3359760bfb
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 17:39:54 -0600

    Item.most_revenue spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,27 @@ describe Item, type: :model do
   it { is_expected.to validate_presence_of(:unit_price) }
 
   it { is_expected.to belong_to(:merchant) }
+
+
+  context 'Methods' do
+    describe '.most_revenue' do
+      it 'should return the X items with the most revenue' do
+        items = create_list(:item, 5)
+        create(:customer)
+        create(:merchant)
+        items.each_with_index do |item, index|
+          invoices = create_list(:invoice, index+1)
+          invoices.each do |invoice|
+            create(:invoice_item, invoice: invoice, item: item)
+          end
+        end
+
+        result = Item.most_revenue(3)
+
+        expect(result.length).to be(3)
+        expect(result.first).to be(items.last)
+        expect(result.last).to be(items[3])
+      end
+    end
+  end
 end
