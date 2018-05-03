ed2aa3d1fbdc253175085d833a00696e49102e5f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 09:27:47 -0600

    Invoice endpoint spec

---
+++ spec/requests/api/v1/invoices/invoices_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Invoices endpoints' do
+  it 'should return a collection of invoices' do
+    create_list(:invoice, 3)
+
+    get '/api/v1/invoices'
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response)
+
+    expect(invoices.length).to be(3)
+  end
+
+  it 'should return a serialized invoice' do
+    invoices = create_list(:invoices, 3)
+
+    get "/api/v1/invoices/#{invoices.first.id}"
+
+    expect(response).to be_successful
+
+    invoice = JSON.parse(invoice)
+
+    expect(invoice.id).to be(invoices.first.id)
+    expect(invoice.created_at).to be_nil
+    expect(invoice.updated_at).to be_nil
+  end
+end
