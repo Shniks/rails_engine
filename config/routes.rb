56511b2240d64e3c927c6244003fa3d2ce97871f
Nikhil Shahi <shniks@gmail.com>
2018-05-03 06:47:55 -0600

    Created passing test for item invoice items relationship.

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#index'
         get '/:id/merchant', to: 'merchant#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
       end
       resources :items, only: [:index, :show]
 
