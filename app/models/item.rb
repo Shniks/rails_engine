2ed753957f478e13dd160f2460ba2601f10f4ac4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 18:09:21 -0600

    Item.most_revenue method

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_revenue(quantity)
+    joins(:invoice_items, :invoices)
+      .where(invoices: {status: 'shipped'})
+      .select('items.*, SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue')
+      .group(:id)
+      .order('revenue DESC')
+      .limit(quantity)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,21 +11,21 @@ describe Item, type: :model do
   context 'Methods' do
     describe '.most_revenue' do
       it 'should return the X items with the most revenue' do
-        items = create_list(:item, 5)
+        merchant = create(:merchant)
+        items = create_list(:item, 5, merchant: merchant)
         create(:customer)
-        create(:merchant)
         items.each_with_index do |item, index|
-          invoices = create_list(:invoice, index+1)
+          invoices = create_list(:invoice, index+1, status: 'shipped')
           invoices.each do |invoice|
             create(:invoice_item, invoice: invoice, item: item)
           end
         end
 
         result = Item.most_revenue(3)
-
+        
         expect(result.length).to be(3)
-        expect(result.first).to be(items.last)
-        expect(result.last).to be(items[3])
+        expect(result.first).to eq(items.last)
+        expect(result.last).to eq(items[-3])
       end
     end
   end
