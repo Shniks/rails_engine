a6c0bb93044722472a32c0fdaacb85c763245d6f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 18:23:35 -0600

    Item.most_sold spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,23 +9,35 @@ describe Item, type: :model do
 
 
   context 'Methods' do
+    before(:each) do
+      merchant = create(:merchant)
+      @items = create_list(:item, 5, merchant: merchant)
+      create(:customer)
+      @items.each_with_index do |item, index|
+        invoices = create_list(:invoice, index+1, status: 'shipped')
+        invoices.each do |invoice|
+          create(:invoice_item, invoice: invoice, item: item)
+        end
+      end
+    end
+
     describe '.most_revenue' do
       it 'should return the X items with the most revenue' do
-        merchant = create(:merchant)
-        items = create_list(:item, 5, merchant: merchant)
-        create(:customer)
-        items.each_with_index do |item, index|
-          invoices = create_list(:invoice, index+1, status: 'shipped')
-          invoices.each do |invoice|
-            create(:invoice_item, invoice: invoice, item: item)
-          end
-        end
-
         result = Item.most_revenue(3)
         
         expect(result.length).to be(3)
-        expect(result.first).to eq(items.last)
-        expect(result.last).to eq(items[-3])
+        expect(result.first).to eq(@items.last)
+        expect(result.last).to eq(@items[-3])
+      end
+    end
+
+    describe '.most_sold' do
+      it 'should return the X items that were most sold' do
+        result = Item.most_sold(2)
+
+        expect(result.length).to be(2)
+        expect(result.first).to eq(@items.last)
+        expect(result.last).to eq(@items[-2])
       end
     end
   end
