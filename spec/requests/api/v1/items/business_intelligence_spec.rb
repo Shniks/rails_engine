5443a93ed7dce2c0d562e4514bbf66da4cf03582
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 18:15:01 -0600

    Item most item endpoint spec

--- spec/requests/api/v1/items/business_intelligence_spec.rb
+++ spec/requests/api/v1/items/business_intelligence_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 describe 'Items Business Intelligence API' do
-  it 'should return the top X items ranked by total revenue' do
+  before(:each) do
     merchant = create(:merchant)
-    items = create_list(:item, 5, merchant: merchant)
+    @items = create_list(:item, 5, merchant: merchant)
     create(:customer)
 
-    items.each_with_index do |item, index|
+    @items.each_with_index do |item, index|
       invoices = create_list(:invoice, index+1, status: 'shipped')
       invoices.each do |invoice|
         create(:invoice_item, invoice: invoice, item: item)
       end
     end
+  end
 
+  it 'should return the top X items ranked by total revenue' do
     get "/api/v1/items/most_revenue?quantity=3"
 
     expect(response).to be_successful
@@ -20,7 +22,19 @@ describe 'Items Business Intelligence API' do
     json = JSON.parse(response.body)
 
     expect(json.length).to be(3)
-    expect(json.first['id']).to be(items.last.id)
-    expect(json.last['id']).to be(items[-3].id)
+    expect(json.first['id']).to be(@items.last.id)
+    expect(json.last['id']).to be(@items[-3].id)
+  end
+
+  it 'should retur the top X items ranked by total number sold' do
+    get "/api/v1/items/most_items?quantity=2"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(json.length).to be(3)
+    expect(json.first['id']).to be(@items.last.id)
+    expect(json.last['id']).to be(@items[-2].id)
   end
 end
