53a460c451a8a530afd04f9cbac3fa303435cae1
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-03 18:35:50 -0600

    Best date controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
         get 'most_items', to: 'item#index'
         get '/:id/merchant', to: 'merchant#show'
         get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/best_day', to: 'best_day#show'
       end
       resources :items, only: [:index, :show]
 
